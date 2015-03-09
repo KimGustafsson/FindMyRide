056bbbd1e5c4eb609952eb48bdcb8eaa1a8495dc
Simon Gullstrand <simon.gullstrand@hotmail.com>
2015-03-09 15:31:51 +0100

    first try

--- java/se/mah/ad1532/findmyride/MainActivity.java
+++ java/se/mah/ad1532/findmyride/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 // anus
+    //banan
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
